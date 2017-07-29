9a5b4341658074d50cdf91ca137d336ecd8e096a
gkazup <gkazup@gmail.com>
2017-07-29 13:50:16 +1000

    modified handle for remote

---
+++ pizero_i2c/05_gamepy_remote_video_v2.py
@@ -0,0 +1,210 @@
+#!/usr/bin/python
+import smbus
+import time
+import sys
+import signal
+import pygame
+import cv2
+import numpy
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
+
+# for RPI version 2, we use bus No. 1
+bus = smbus.SMBus(1)
+
+# This is the target address we setup in the Arduino Program
+address = 0x08
+
+
+# send string to the i2c wire
+def writeWireString(value):
+    byte_list = []
+    count = len(value)
+    if (count > 1):
+        for index in range(1,(count)):
+            byte_list.append(ord(value[index]))
+    print count,ord(value[0]),byte_list
+    try:
+        bus.write_i2c_block_data(address, ord(value[0]), byte_list)
+    except IOError:
+        print "Error: IOError occured while sending"
+    return -1
+
+
+# read number of bytes from the i2c wire
+# and return it as a string
+def readWireString(numbytes):
+    byte_list = []
+    data = ""
+    try:
+        byte_list = bus.read_i2c_block_data(address, 0, numbytes)
+    except IOError:
+        print "Error: IOError occured while receiving"
+    print byte_list
+    for byte in byte_list:
+        if (byte == 0):
+            return data
+        data = data + chr(byte)
+    return data
+
+
+# read number of bytes from the i2c wire
+# and return them as byte list
+def readWire(numbytes):
+    byte_list = []
+    try:
+        byte_list = bus.read_i2c_block_data(address, 0, numbytes)
+    except IOError:
+        print "Error: IOError occured while receiving"
+    return byte_list
+
+# CTRL+C signal handler 
+def signal_handler(signal, frame):
+    print '...Exiting...'
+    sys.exit(0)
+
+# handle keypresses
+def handle_keypress(input):
+    going = True
+    change = False
+    output = 0
+
+    if (1 == (input & 1)):
+        left = True
+    else:
+        left = False
+    if (2 == (input & 2)):
+        right = True
+    else:
+        right = False
+    if (4 == (input & 4)):
+        up = True
+    else:
+        up = False
+    if (8 == (input & 8)):
+        down = True
+    else:
+        down = False
+
+    for e in pygame.event.get():
+        if (e.type == pygame.QUIT):
+            going = False
+        if (e.type == pygame.KEYDOWN):
+            if (e.key == pygame.K_ESCAPE):
+                going = False
+            elif (e.key == pygame.K_LEFT):
+                left = True
+                change = True
+            elif (e.key == pygame.K_RIGHT):
+                right = True
+                change = True
+            elif (e.key == pygame.K_UP):
+                up = True
+                change = True
+            elif (e.key == pygame.K_DOWN):
+                down = True
+                change = True
+        if e.type == pygame.KEYUP:
+            if (e.key == pygame.K_LEFT):
+                left = False
+                change = True
+            elif (e.key == pygame.K_RIGHT):
+                right = False
+                change = True
+            elif (e.key == pygame.K_UP):
+                up = False
+                change = True
+            elif (e.key == pygame.K_DOWN):
+                down = False
+                change = True
+
+    if (change):
+        if (up and left):
+            writeWireString("tf 50 150")
+        elif (up and right):
+            writeWireString("tf 150 50")
+        elif (down and left):
+            writeWireString("tb 50 150")
+        elif (down and right):
+            writeWireString("tb 150 50")
+        elif (up):
+            writeWireString("f 150")
+        elif (down):
+            writeWireString("b 150")
+        elif (left):
+            writeWireString("tl 150 150")
+        elif (right):
+            writeWireString("tr 150 150")
+        if ( (not up) and (not down) and (not left) and (not right) ):
+            writeWireString("s")
+
+    if (left):
+        output += 1
+    if (right):
+        output += 2
+    if (up):
+        output += 4
+    if (down):
+        output += 8
+    if (going):
+        output += 16
+    return output
+
+def getCamFrame(camera, rawCapture):
+    camera.capture(rawCapture, format="bgr")
+    rawframe = rawCapture.array
+    #cv2.imshow("Image", rawframe)
+    #cv2.waitKey(0) & 0xFF
+    frame = cv2.cvtColor(rawframe,cv2.COLOR_BGR2RGB)
+    frame = numpy.rot90(frame,2)
+    frame = numpy.fliplr(frame)
+    return frame
+
+def blitCamFrame(frame, screen):
+    frame = pygame.surfarray.make_surface(frame)
+    screen.blit(frame,(0,0))
+    return screen
+
+
+# MAIN part
+signal.signal(signal.SIGINT, signal_handler)
+
+print "Initalizing..."
+pygame.init()
+pygame.key.set_repeat()
+screen_width, screen_height = 320, 208
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption("Video Control")
+
+camera = PiCamera()
+camera.resolution = ((screen_width,screen_height))
+#camera.framerate = 16
+rawCapture = PiRGBArray(camera)
+
+print "press ESC to exit"
+keyinfo = 0
+going = True
+writeWireString("e")
+
+counter = 1
+while going:
+    keyinfo = handle_keypress(keyinfo)
+    if (16 == (keyinfo & 16)):
+        going = True
+    else:
+        going = False
+    if (counter == 1000):
+        frame = getCamFrame(camera,rawCapture)
+        screen = blitCamFrame(frame, screen)
+        pygame.display.flip()
+        rawCapture.truncate(0)
+        counter = 0
+    counter += 1
+    #pygame.time.wait(10)
+
+# cleanup
+writeWireString("s")
+writeWireString("d")
+quit()
+
